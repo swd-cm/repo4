aebcaf91aa6a40a58573323e5272b0b9096cadbb
admin <admin>
2019-09-08 19:46:03 +0800

    更改项目

--- maven_git_java/src/main/java/com/swd/cm.java
+++ maven_git_java/src/main/java/com/swd/cm.java
@@ -7,5 +7,7 @@ package com.swd;
 public class cm {
     public static void main(String[] args) {
         System.out.println("swd9999");
+        System.out.println("swdswdswd");
+        System.out.println();
     }
 }
